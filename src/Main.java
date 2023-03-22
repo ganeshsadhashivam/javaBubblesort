4bbd628009df08b2c27fb6e81961312f63798fcb
ganesh <ganeshsadhashivam@outlook.com>
2023-03-22 16:25:36 +0530

    bubble sort

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+    //int [] arr={-1,7,-32,89};
+        int [] arr={1,3,4,2,5};
+    bubble(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void bubble(int[] arr){
+        boolean swapped;
+        //run the steps n-1 times
+
+        for (int i = 0; i < arr.length; i++) {
+
+    swapped = false;
+
+            //for each step max item will come at the last respective index
+            for (int j = 1; j < arr.length - i; j++) {
+                //swap if the item is smaller than the previous item
+                if (arr[j] < arr[j - 1]) {
+                    //swap
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                    swapped = true;
+                }
+            }
+            //if did not swap
+            if(!swapped){
+                break;
+            }
+        }
+    }
+}
+
